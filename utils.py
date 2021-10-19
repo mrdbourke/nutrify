cec6e7e4d9fa71262ecca7f8004a92e71c94126d
Daniel Bourke <danielbourke@icloud.com>
2021-10-19 17:09:42 +1000

    clean up code

--- utils.py
+++ utils.py
@@ -3,37 +3,39 @@ import streamlit as st
 import os
 from google.cloud import storage
 
-### Uploader function to Google Storage ###
+
 # Bucket ID for Google Storage upload
 if os.environ.get("TEST_NUTRIFY_ENV_VAR"):
-    print("***Using test storage bucket***")
-    BUCKET_ID = "food-vision-images-test-upload" # test bucket
+    BUCKET_ID = "food-vision-images-test-upload"  # test bucket
+    print(f"***Using test Google Storage bucket: {BUCKET_ID}***")
 else:
-    BUCKET_ID = "food-vision-images" # prod bucket
+    BUCKET_ID = "food-vision-images"  # prod bucket
+
 
 def upload_blob(source_file_name, destination_blob_name):
     """
-    Uploads image file to Google Storage bucket. 
+    Uploads image file to Google Storage bucket.
     """
     # Path to file
-    # source_file_name = 
+    # source_file_name =
     # ID of GCS object to store
     print("Starting to try and upload...")
 
-    # Authenticate storage, see: https://cloud.google.com/docs/authentication/production#passing_code 
-    storage_client = storage.Client.from_service_account_info(st.secrets["GOOGLE_APPLICATION_CREDENTIALS"])
+    # Authenticate storage,
+    # see: https://cloud.google.com/docs/authentication/production#passing_code
+    storage_client = storage.Client.from_service_account_info(
+        st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
+    )
     bucket = storage_client.bucket(bucket_name=BUCKET_ID)
     blob = bucket.blob(destination_blob_name)
 
     # Upload object
     blob.upload_from_file(source_file_name, rewind=True)
 
-    print(
-        f"File {source_file_name} uploaded to {destination_blob_name}"
-    )
+    print(f"File {source_file_name} uploaded to {destination_blob_name}")
 
-### Create a unique file name ID for saving images ###
-def create_unique_filename():
+
+def create_unique_filename() -> str:
     """
     Creates a unique filename for storing uploaded images.
     """
